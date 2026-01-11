d84fd20cdba0d5481e2a62e291af7b239a56740a
ValentinMICHELiutnfc <valentin.michel03@edu.univ-fcomte.fr>
2026-01-11 14:34:58 +0100

    Update BankAccountView.vue

--- src/views/BankAccountView.vue
+++ src/views/BankAccountView.vue
@@ -1,61 +1,97 @@
 <template>
   <div>
-    <h1>Account data</h1>
+    <h1>Données du compte</h1>
 
-    <span>account number</span><input v-model="number"><button @click="resetAccountNumber">Reset</button>
+    <span>Numéro de compte : </span>
+    <input v-model="number" placeholder="FR...">
+    <button @click="reset">Reset</button>
     <br />
-    <button :disabled="!isAccountNumberValid" @click="bankStore.getAccountAmount(number)">Get amount</button>
-    <button :disabled="!isAccountNumberValid" @click="bankStore.getAccountTransactions(number)">Get transactions</button>
-    <p v-if="bankStore.accountNumberState===-1">invalid account number</p>
+
+    <button :disabled="!isAccountNumberValid" @click="fetchAccount">
+      Valider
+    </button>
+
+    <p v-if="errorMsg" style="color: red;">{{ errorMsg }}</p>
+
     <hr />
-    <span>available amount : </span>
-    <span v-if="bankStore.accountNumberState === 1" >{{bankStore.accountAmount}}</span>
-    <span v-else></span>
-    <hr />
-    <p>passed transactions:</p>
-    <div v-if="bankStore.accountNumberState === 1">
+
+    <div v-if="bankStore.account">
+      
+      <span>Solde disponible : </span>
+      <span :style="{ color: bankStore.account.amount >= 0 ? 'green' : 'red', fontWeight: 'bold' }">
+        {{ bankStore.account.amount }} €
+      </span>
+      
+      <hr />
+      
+      <p>Transactions passées :</p>
       <ul>
-        <li v-for="(trans,index) in bankStore.accountTransactions" :key="index">{{trans.amount}} the {{convertDate(trans.date.$date)}}</li>
+        <li v-for="(trans, index) in bankStore.transactions" :key="trans.uuid || index">
+          {{ trans.amount }} € 
+          le {{ convertDate(trans.date.$date || trans.date) }}
+        </li>
       </ul>
     </div>
-    <span v-else></span>
+    
+    <div v-else-if="!errorMsg && number">
+      <p>Veuillez valider le numéro de compte.</p>
+    </div>
 
   </div>
-
 </template>
 
 <script setup>
-/* ***************************
-  IMPORTS
- *************************** */
-import {ref, computed} from "vue";
-import {useBankStore} from "@/stores/bank.js";
+import { ref, computed } from "vue";
+import { useBankStore } from "@/stores/bank.js";
 
-/* ***************************
-  STATE
- *************************** */
-const number = ref('')
-const bankStore = useBankStore()
+// --- STATE ---
+const number = ref('FRBADORG78901234567890-0000001'); // Valeur par défaut pour tester plus vite
+const bankStore = useBankStore();
+const errorMsg = ref('');
 
-/* ***************************
-  COMPUTED
- *************************** */
+// --- COMPUTED ---
 const isAccountNumberValid = computed(() => {
-  const rexp = RegExp('^[A-Za-z0-9]{22}-[0-9]{7}$','g')
-  return rexp.test(number.value)
-})
+  // Regex pour vérifier le format du compte
+  const rexp = /^[A-Za-z0-9]{22}-[0-9]{7}$/g;
+  return rexp.test(number.value);
+});
 
-/* ***************************
-  FUNCTIONS
- *************************** */
-function convertDate(date) {
-  let d = new Date(date)
-  return d.getMonth()+"/"+d.getDate()+"/"+d.getFullYear()+" the "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()
+// --- FUNCTIONS ---
+
+async function fetchAccount() {
+  errorMsg.value = '';
+  
+  // On appelle la méthode du store mise à jour précédemment
+  const response = await bankStore.getAccount(number.value);
+  
+  if (response.error !== 0) {
+    errorMsg.value = response.data || "Erreur inconnue";
+  }
 }
 
-function resetAccountNumber() {
-  number.value = ''
-  bankStore.resetAccountNumberState()
+function convertDate(dateVal) {
+  const d = new Date(dateVal);
+  return d.toLocaleDateString() + " à " + d.toLocaleTimeString();
 }
 
+function reset() {
+  number.value = '';
+  errorMsg.value = '';
+  bankStore.logout(); // Vide le store
+}
 </script>
+
+<style scoped>
+input {
+  margin: 0 10px;
+  padding: 5px;
+}
+button {
+  margin-right: 5px;
+  cursor: pointer;
+}
+button:disabled {
+  cursor: not-allowed;
+  opacity: 0.5;
+}
+</style>
